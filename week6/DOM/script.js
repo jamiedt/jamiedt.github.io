e86f3107c6361f65c95e26985d3403c61f7568c3
jamiedt <s4092802@student.rmit.edu.au>
2025-04-10 12:03:26 +1000

    week 6

---
+++ week6/DOM/script.js
@@ -0,0 +1,10 @@
+const top_heading = document.querySelector("h1");
+console.log(top_heading);
+
+const first_p = document.querySelector("p");
+console.log(first_p);
+
+const first_sub_heading = document.querySelector("#first_subheading");
+console.log(first_sub_heading);
+console.log(first_sub_heading.textContent);
+first_sub_heading.textContent = "woah look at this";
