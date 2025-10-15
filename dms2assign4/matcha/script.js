0ab69b409a923f1a3023479d03d2a0a31bb525c3
jamiedt <s4092802@student.rmit.edu.au>
2025-10-16 00:57:16 +1100

    Update script.js

--- dms2assign4/matcha/script.js
+++ dms2assign4/matcha/script.js
@@ -33,6 +33,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentCols = 0;
   let currentRows = 0;
 
+  gameScreen.style.display = "none";
+
   function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
